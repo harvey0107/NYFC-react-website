96d62f953ec6641df7a4e9442e49a7fc0b6c3695
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-02-20 21:47:04 -0500

    botton

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './MainSection.css'
+import { Button } from 'antd';
 export const MainSection = () => {
     return (
         <div className='main-container'>
@@ -7,6 +8,7 @@ export const MainSection = () => {
                 <h1 className='main-headline'>Train Insane or Remain the Same</h1>
                 <div className='main-line'></div>
                 <p className='main-intro'>We work hard to maintain the values that we established in 1973, when we were just a small chain of squash clubs. We’re consistently improving our offerings and services, and pride ourselves on creating connections with our members and delivering the most personalized fitness experiences possible.</p>
+                <Button type="primary">Primary Button</Button>
             </div>
         </div>
     )
--- src/components/Menultems.js
+++ src/components/Menultems.js
@@ -5,7 +5,7 @@ export const MenuItems = [
         cName: 'nav-links'
     },
     {
-        title: 'About',
+        title: 'About Us',
         url: '#',
         cName: 'nav-links'
     },
