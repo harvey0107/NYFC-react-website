8010ace695a012d11c366327c90752255eb84a9d
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-02-20 20:27:18 -0500

    main line

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -5,6 +5,7 @@ export const MainSection = () => {
         <div className='main-container'>
             <div className='main-textbox'>
                 <h1 className='main-headline'>Train Insane or Remain the Same</h1>
+                <div className='main-line'></div>
                 <p className='main-intro'>We work hard to maintain the values that we established in 1973, when we were just a small chain of squash clubs. We’re consistently improving our offerings and services, and pride ourselves on creating connections with our members and delivering the most personalized fitness experiences possible.</p>
             </div>
         </div>
