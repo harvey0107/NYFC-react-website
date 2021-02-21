4648c2a860419e7317e608617f2cc052e9edc8a6
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-02-20 22:11:40 -0500

    bottom

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -8,7 +8,7 @@ export const MainSection = () => {
                 <h1 className='main-headline'>Train Insane or Remain the Same</h1>
                 <div className='main-line'></div>
                 <p className='main-intro'>We work hard to maintain the values that we established in 1973, when we were just a small chain of squash clubs. We’re consistently improving our offerings and services, and pride ourselves on creating connections with our members and delivering the most personalized fitness experiences possible.</p>
-                <Button type="primary">Primary Button</Button>
+                <Button type="primary" className='main-button'>Learn More</Button>
             </div>
         </div>
     )
