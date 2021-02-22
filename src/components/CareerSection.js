b013d29d839e10ac4f4e30ed164ceea954182148
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-02-22 15:29:05 -0500

    career

---
+++ src/components/CareerSection.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import'./CareerSection.css'
+export const CareerSection = () => {
+    return (
+        <div className='career-container'>
+            <div className='career-textbox'>
+                <h1 className='career-headline'>Career</h1>
+                <div className='career-intro'>
+                    <p className='career-text'>What began as an opportunity to bring the immensely popular sport of squash to the New York masses has evolved and expanded to a network of over 150 clubs catering to over half a million members across eight states and two countries (including Switzerland!). Dedicated to offering our members the best possible experience, we’ve proudly been at the forefront of fitness for over four decades while still maintaining that familiar, neighborhood feel.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default CareerSection
