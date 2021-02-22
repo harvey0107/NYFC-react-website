6bc4e24bb77e860e8535506ee22b9f077651c689
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-02-22 14:16:18 -0500

    position

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar2 from './components/Navbar2'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
+import AboutUs from './components/pages/AboutUs';
 
 function App() {
   return (
     <>
     <Router>
+      <AboutUs />
       <Switch>
         <Route path='/' exact component=
         {Home} />
