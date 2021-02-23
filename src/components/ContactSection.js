3b16bd5b57e7bc341c163d8a6aa1db0ec4df75fc
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-02-22 19:09:58 -0500

    contact

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -2,15 +2,16 @@ import React from 'react'
 import'./ContactSection.css'
 export const ContactSection = () => {
     return (
-        <div className='contact-container'>
-            <div className='contact-textbox'>
+        <div className='contact-container tc'>
+            <div className='contact-textbox tc'>
                 <h1 className='contact-headline'>Contact</h1>
-                <div className='contact-intro'>
-                    <p className='contact-text'>
-                    How can we help? Ask the experts! Hours of Operation – 9AM to 5PM, 
-                    Headquarters: 888.123.1123. 
-                    Service Department: 800 NYFC FIXX or service@gnyfc.com.
-                    Local Store: 800-496-7687 or sales@nyfc.com.</p>
+                <div className='contact-intro '>
+                    <p className='contact-text ' >
+                    How can we help? Ask the experts! 
+                    Hours of Operation: 9AM to 5PM, 
+                    Headquarters: 888.123.1123
+                    Local Store: 800-496-7687 or sales@nyfc.com
+                    Service Department: 800 NYFC FIXX or service@gnyfc.com</p>
                 </div>
             </div>
         </div>
