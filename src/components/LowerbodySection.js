6c891c58fa5bd5c7d569f79d6eee41dab3af2732
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-02-26 11:29:32 -0500

    added lower page

---
+++ src/components/LowerbodySection.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import LowerData from './LowerData'
+import LisOfLower from "./ListOfLower";
+
+
+class LowerbodySection extends React.Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            collections: LowerData
+        } 
+    }
+    render() { 
+        const {collections} =this.state
+        return (<div className='lower-workout-page'>
+                {
+                    collections.map(({ id, ...otherProps}) => (
+                        <LisOfLower key={id} {...otherProps} />
+                    ))
+                }
+            </div>)
+    }
+}
+
+export default LowerbodySection
