1d7999f9a959efb4d7aae96f48d7d011983b7fd3
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-02-27 23:26:57 -0500

    mobile nav

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -5,33 +5,65 @@ import {MenuItems} from './Menultems'
 
 import './Navbar2.css'
 
-class Navbar2 extends Component{
-    state ={clicked: false}
+const DesktopNav = () => {
+    return (
+        <nav className='NavbarItem'>
+            <Link to='/'>
+                <i className="fas fa-dumbbell"></i>
+                <h1 className='navbar-logo'>NYFC</h1>
+            </Link>
+            {MenuItems.map((item, index) => {
+                return(
+                    <li key={index}>
+                        <a className={item.cName} href={item.url}>
+                        {item.title}</a>
+                    </li>
+                )
+            })}  
+        </nav>
+    )
+}
+
+class MobileNav extends Component {
+    state = {
+        clicked: false,
+    }
+
     handleClick=()=>{
         this.setState({clicked: !this.state.clicked})
     }
-    render(){
-        return(
-            <nav className='NavbarItem'>
-                <Link to='/'>
-                    <i className="fas fa-dumbbell"></i>
-                    <h1 className='navbar-logo'>NYFC</h1>
-                </Link>
+       
+    render () {
+        return (
+            <div className="navbar-item">
                 <div className='menu-icon' onClick={this.handleClick}>
                     <i className={this.state.clicked? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {MenuItems.map((item, index) => {
-                        return(
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                {item.title}</a>
-                            </li>
-                        )
-                    })}  
-                </ul>
+            </div>
+        )
+    }
+}
 
-            </nav>
+class Navbar2 extends Component{
+    state ={
+        clicked: false,
+        windowWidth: 0,
+    }
+    handleClick=()=>{
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    // event listener, resize event
+
+    // check widow width, set state
+
+    // component did mount, check window width
+
+    render(){ 
+        return(
+           <>
+            {this.state.windowWidth < 760 ? <MobileNav /> : <DesktopNav />}
+           </>
         )
     }
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import MainSection from '../MainSection';
 import Footer from '../Footer';
-import Navbar2 from '../Navbar2';
 
 function Home () {
     return(
